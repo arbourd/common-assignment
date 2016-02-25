d6029c0d1c683b27955327b781692b7e9bd2274c
Dylan Arbour <dylan.arbour@gmail.com>
2016-02-24 19:59:28 -0500

    Rename files

---
+++ client/index.js
@@ -0,0 +1,25 @@
+var handler = StripeCheckout.configure({
+  key: 'pk_test_piC2edrOoqf3aMu9nsEteq4h',
+  image: '//img/documentation/checkout/marketplace.png',
+  locale: 'auto',
+  token: function(token) {
+    // Use the token to create the charge with a server-side script.
+    // You can access the token ID with `token.id`
+  }
+});
+
+$('#customButton').on('click', function(e) {
+  // Open Checkout with further options
+  handler.open({
+    name: 'Demo Site',
+    description: '2 widgets',
+    currency: "cad",
+    amount: 2000
+  });
+  e.preventDefault();
+});
+
+// Close Checkout on page navigation
+$(window).on('popstate', function() {
+  handler.close();
+});
